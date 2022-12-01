ee7614ec009e5c2d4b1167ca7222d3a143b41534
Rahadul8090 <rahadulislam8090@gmail.comm>
2022-12-02 00:19:25 +0600

    add user and bayers

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
+require('dotenv').config()
 
-// password: fW6KsnOGrxTLbr2E
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = "mongodb+srv://assignment-12:fW6KsnOGrxTLbr2E@cluster0.qzaiqpb.mongodb.net/?retryWrites=true&w=majority";
+
+const uri = `mongodb+srv://${process.env.MONGO_DB_NAME}:${process.env.MONGO_DB_PASSWORD}@cluster0.qzaiqpb.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+// console.log(uri)
 function run() {
   try {
     const categoreData = client.db('categories').collection('categories-name')
     const categoreProdect = client.db('categories').collection('categories-prodect')
     const oredersCategore = client.db('categories').collection('oreders')
+    const AllUsersAdmin = client.db('categories').collection('allusers')
 
     app.get('/categore', async (req, res) => {
       const quriy = {}
@@ -54,7 +57,18 @@ function run() {
       res.send(result)
     })
 
-
+    app.get('/allusers', async (req,res)=>{
+      const role = req.query.role
+      const query ={role : role}
+      const carsor = AllUsersAdmin.find(query)
+      const result = await  carsor.toArray()
+      res.send(result)
+    })
+    app.post('/allusers', async (req, res) => {
+      const quriy = req.body
+      const result = await AllUsersAdmin.insertOne(quriy)
+      res.send(result)
+    })
    
   }
   finally {
